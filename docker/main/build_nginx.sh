6f5dad45fa482052c5e960723e4433f346620459
nanguantong <zww0602jsj@gmail.com>
2024-08-09 11:43:00 +0800

    Change apt sources.list

--- docker/main/build_nginx.sh
+++ docker/main/build_nginx.sh
@@ -2,6 +2,10 @@
 
 set -euxo pipefail
 
+sed -i s/deb.debian.org/mirrors.tuna.tsinghua.edu.cn/g /etc/apt/sources.list && \
+sed -i 's|security.debian.org|mirrors.tuna.tsinghua.edu.cn/|g' /etc/apt/sources.list && \
+sed -i '/stretch-updates/d' /etc/apt/sources.list && \
+
 NGINX_VERSION="1.25.3"
 VOD_MODULE_VERSION="1.31"
 SECURE_TOKEN_MODULE_VERSION="1.5"
--- docker/main/install_deps.sh
+++ docker/main/install_deps.sh
@@ -2,6 +2,10 @@
 
 set -euxo pipefail
 
+sed -i s/deb.debian.org/mirrors.tuna.tsinghua.edu.cn/g /etc/apt/sources.list && \
+sed -i 's|security.debian.org|mirrors.tuna.tsinghua.edu.cn/|g' /etc/apt/sources.list && \
+sed -i '/stretch-updates/d' /etc/apt/sources.list && \
+
 apt-get -qq update
 
 apt-get -qq install --no-install-recommends -y \
